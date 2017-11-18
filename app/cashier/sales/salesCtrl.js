9040bb597a6b37d6dc455e532d656a968efa9355
kevin <kevinalfa17@gmail.com>
2017-11-18 00:17:07 -0600

    sales adding codebar system ready

--- app/cashier/sales/salesCtrl.js
+++ app/cashier/sales/salesCtrl.js
@@ -1,138 +1,241 @@
 'use strict';
 angular.module('newApp')
-    .controller('salesCtrl', ['$scope', 'pluginsService', '$window', '$route','$http','config','$rootScope',
-     function ($scope, pluginsService, $window, $route,$http,config,$rootScope) {
-        $scope.$on('$viewContentLoaded', function () {
+    .controller('salesCtrl', ['$scope', 'pluginsService', '$window', '$route', '$http', 'config', '$rootScope',
+        function ($scope, pluginsService, $window, $route, $http, config, $rootScope) {
+            $scope.$on('$viewContentLoaded', function () {
 
-            $scope.user = {
-                first_name: '',
-                first_last_name: ''
-            };
+                $scope.user = {
+                    first_name: '',
+                    first_last_name: ''
+                };
 
-            $scope.defaultClient = true;
-            $scope.clientExist = true;
-            $scope.creditCard = false;
-            $scope.userId = '';
-            $scope.idReady = false;
-            $scope.productList = [];
-            $scope.quantity = 1;
-            $scope.total = 0;
-            $scope.cash = '';
+                $scope.defaultClient = true;
+                $scope.clientExist = true;
+                $scope.creditCard = false;
+                $scope.userId = '';
+                $scope.idReady = false;
+                $scope.productList = [];
+                $scope.quantity = 1;
+                $scope.total = 0;
+                $scope.cash = '';
+                $scope.barcode = '';
+                $scope.lowStock = false;
 
-            //Subsidiary id
-            $scope.subsidiary = $rootScope.globals.currentUser.subsidiary;
-            console.log("subsi"+ $scope.subsidiary)
-            
+                //Subsidiary id
+                $scope.subsidiary = $rootScope.globals.currentUser.subsidiary;
+                console.log("subsi" + $scope.subsidiary)
 
-            //Remove item to the list
-            $scope.remove = function (index) {
-                $scope.total = $scope.total - $scope.productList[index].total; //Remove product cost to total cost                
-                $scope.productList.splice(index, 1)
-            }
 
-            //Add new item to list
-            $scope.add = function () {
-                var product = {};
-                product.name = "newname"; //Search in product list
-                product.quantity = $scope.quantity;
-                product.price = 100; ////Search in product list
+                //Remove item to the list
+                $scope.remove = function (index) {
+                    $scope.total = $scope.total - ($scope.productList[index].price*$scope.productList[index].quantity); //Remove product cost to total cost                
+                    $scope.productList.splice(index, 1)
+                }
 
-                $scope.productList.push(product);
-                $scope.total = $scope.total + product.price * product.quantity; //Add product cost to total cost
-            }
+                //Add new item to list
+                $scope.add = function () {
+                    //Check if is already added
+                    if ($scope.productAlreadyAdded($scope.barcode)) {
+                        $scope.increaseProductQuantity($scope.barcode);
+                    } else {
+                        $scope.addProduct($scope.barcode);
+                    }
 
-            //Finish sale and print invoice
-            $scope.finish = function () {
-                var $popup = $window.open("sales/invoice.html", "popup", "width=450,height=600,left=10,top=150");
-                $popup.productList = $scope.productList;
-                $popup.date = new Date();
-                $popup.total = $scope.total;
-                $popup.number = "0000"; //Get from DB
-                $route.reload();
+                }
 
-            }
+                //Finish sale and print invoice
+                $scope.finish = function () {
+                    var $popup = $window.open("sales/invoice.html", "popup", "width=450,height=600,left=10,top=150");
+                    $popup.productList = $scope.productList;
+                    $popup.date = new Date();
+                    $popup.total = $scope.total;
+                    $popup.number = "0000"; //Get from DB
+                    $route.reload();
 
-            //Get client list to check if client is new 
-            $scope.clients = [];
-            $http.get(config.ip + '/api/Clients')
-                .success(function (result) {
-                    $scope.clients = result;
-                })
-                .error(function (data, status) {
-                    console.log(data);
-                });
+                }
 
-            //Check if specific client exist in client list
-            $scope.checkClient = function(client){
-                for(var i = 0; i<$scope.clients.length; i++){
-                    if($scope.clients[i].id_client == client){
-                        return true;
+                //Get client list to check if client is new 
+                $scope.clients = [];
+                $http.get(config.ip + '/api/Clients')
+                    .success(function (result) {
+                        $scope.clients = result;
+                    })
+                    .error(function (data, status) {
+                        console.log(data);
+                    });
+
+                //Get products that can be added to the sale
+                $scope.products = [];
+                $http.get(config.ip + '/api/MedicinesbySubsidiaries/' + $scope.subsidiary + "?type=s")
+                    .success(function (result) {
+                        $scope.products = result;
+                    })
+                    .error(function (data, status) {
+                        console.log(data);
+                    });
+
+                $scope.productsData = [];
+                $http.get(config.ip + '/api/Medicines/')
+                    .success(function (result) {
+                        $scope.productsData = result;
+                    })
+                    .error(function (data, status) {
+                        console.log(data);
+                    });
+
+
+                //Check if specific client exist in client list
+                $scope.checkClient = function (client) {
+                    for (var i = 0; i < $scope.clients.length; i++) {
+                        if ($scope.clients[i].id_client == client) {
+                            return true;
+                        }
+                    }
+                    return false;
+                }
+
+                //Check if specific client exist in client list
+                $scope.addProduct = function (product) {
+                    for (var i = 0; i < $scope.products.length; i++) {
+                        if ($scope.products[i].medicine == product) {
+                            $scope.searchProduct(product);       
+                        }
                     }
                 }
-                return false;
-            }
 
-            //Aux class
-            $scope.viewPort = $('.sf-viewport');
+                //Search for product name and price
+                $scope.searchProduct = function (product) {
+                    for (var i = 0; i < $scope.productsData.length; i++) {
+                        if ($scope.productsData[i].id_medicine == product) {
+                            var newProduct = $scope.productsData[i];
+                            newProduct.quantity = $scope.quantity;
+                            //Add product to list
+                            $scope.productList.push(newProduct);
+                            //Update total
+                            $scope.total = $scope.total + newProduct.price * newProduct.quantity; //Add product cost to total cost
 
-            $scope.$watch('creditCard', function (newValue, oldValue) {
-                console.log("credit");
-            })
-
-            //Check if customer is a registered client or default client (check control)
-            $scope.$watch('defaultClient', function (newValue, oldValue) {
-
-                //Resize aux class based on if new user form is visible or not
-                if ($scope.clientExist) {
-                    $('.sf-viewport').height(150);
-                } else {
-                    $('.sf-viewport').height(600);
+                        }
+                    }
                 }
 
-            })
-
-            //Check if customer is a registered client or new client is necesary
-            $scope.$watch('clientExist', function (newValue, oldValue) {
-                //Resize aux class based on if new user form is visible or not
-                if ($scope.clientExist) {
-                    $('.sf-viewport').height(150);
-                } else {
-                    $('.sf-viewport').height(600);
+                //Check is product was added to product list before
+                $scope.productAlreadyAdded = function (product) {
+                    for (var i = 0; i < $scope.productList.length; i++) {
+                        if ($scope.productList[i].id_medicine == product) {
+                            return true;
+                        }
+                    }
+                    return false;
                 }
 
-            })
+                //Increase product quantity when already exist
+                $scope.increaseProductQuantity = function (product) {
+                    for (var i = 0; i < $scope.productList.length; i++) {
+                        if ($scope.productList[i].id_medicine == product) {
+                            $scope.productList[i].quantity = $scope.productList[i].quantity + $scope.quantity;
+                             //Update total
+                             $scope.total = $scope.total + $scope.productList[i].price * $scope.quantity; //Add product cost to total cost
+                        }
+                    }
+                }
+
+                //Get product min stock, returns -1 if product doesnt exist
+                 $scope.getMinStock = function (product) {
+                    for (var i = 0; i < $scope.products.length; i++) {
+                        if ($scope.products[i].medicine == product) {
+                            return $scope.products[i].stock_minimo;
+                        }
+                    }
+                    return -1;
+                }
 
 
-            //Watch Client Id to check if exist
-            $scope.$watch('userId', function (newValue, oldValue) {
-                //If data is undefined then mask is not completed
-                if ($scope.userId !== undefined) {
-                    $scope.user.id = $scope.userId;
+                //Aux class
+                $scope.viewPort = $('.sf-viewport');
 
-                    //Check if Id complete (9 numbers)
-                    console.log("idlenght" + $scope.userId.length)
-                    if ($scope.userId.length == 9) {
+                $scope.$watch('creditCard', function (newValue, oldValue) {
+                    console.log("credit");
+                })
 
-                        //Check if exist
-                        $scope.clientExist = $scope.checkClient($scope.userId); //(Change id comparisson with DB search)
-                        //Form validation
-                        $scope.idReady = true;
+                $scope.$watch('quantity', function (newValue, oldValue) {
+                    //Check product min stock
+                    var min = $scope.getMinStock($scope.barcode);
+                    //If product exist and quantity is higher than the min stock then show warning
+                    if(min != -1 && $scope.quantity >= min){
+                        $scope.lowStock = true;
+                    }
+                    else{
+                        $scope.lowStock = false;
+                    }
+                })
 
+                $scope.$watch('barcode', function (newValue, oldValue) {
+                    //Check product min stock
+                    var min = $scope.getMinStock($scope.barcode);
+                    //If product exist and quantity is higher than the min stock then show warning
+                    if(min != -1 && $scope.quantity >= min){
+                        $scope.lowStock = true;
+                    }
+                    else{
+                        $scope.lowStock = false;
+                    }
+                })
+
+                //Check if customer is a registered client or default client (check control)
+                $scope.$watch('defaultClient', function (newValue, oldValue) {
+
+                    //Resize aux class based on if new user form is visible or not
+                    if ($scope.clientExist) {
+                        $('.sf-viewport').height(150);
                     } else {
-                        //Not show new client inputs at the begining
+                        $('.sf-viewport').height(600);
+                    }
+
+                })
+
+                //Check if customer is a registered client or new client is necesary
+                $scope.$watch('clientExist', function (newValue, oldValue) {
+                    //Resize aux class based on if new user form is visible or not
+                    if ($scope.clientExist) {
+                        $('.sf-viewport').height(150);
+                    } else {
+                        $('.sf-viewport').height(600);
+                    }
+
+                })
+
+
+                //Watch Client Id to check if exist
+                $scope.$watch('userId', function (newValue, oldValue) {
+                    //If data is undefined then mask is not completed
+                    if ($scope.userId !== undefined) {
+                        $scope.user.id = $scope.userId;
+
+                        //Check if Id complete (9 numbers)
+                        console.log("idlenght" + $scope.userId.length)
+                        if ($scope.userId.length == 9) {
+
+                            //Check if exist
+                            $scope.clientExist = $scope.checkClient($scope.userId); //(Change id comparisson with DB search)
+                            //Form validation
+                            $scope.idReady = true;
+
+                        } else {
+                            //Not show new client inputs at the begining
+                            $scope.clientExist = true;
+                            //Form validation
+                            $scope.idReady = false;
+                        }
+                    } else {
+                        //Not show new client inputs if masked is incomplete (ID is less than 9)
                         $scope.clientExist = true;
                         //Form validation
                         $scope.idReady = false;
                     }
-                } else {
-                    //Not show new client inputs if masked is incomplete (ID is less than 9)
-                    $scope.clientExist = true;
-                    //Form validation
-                    $scope.idReady = false;
-                }
+
+                });
 
             });
 
-        });
-
-    }]);
+        }
+    ]);
